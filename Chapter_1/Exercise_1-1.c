9df97a5cce3a9c72aafbb1d8f60c03a646dbba1b
Sameer Achhab <sameerkw23@gmail.com>
2022-10-03 00:22:35 +0530

    Improved program. See commit description for more information.
    This commit adds :-
    1. Why don't you go ask your mom
    2. Why are you still here?

--- Chapter_1/Exercise_1-1.c
+++ Chapter_1/Exercise_1-1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
-//Hello World
-int main(){
-  printf("Hello World\n");
-  return 0;
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+char __rand_char(){
+	char *l="abcdefghijklmnopqrstuvwxyz";
+	return l[rand() % 26];
 }
-
+void randomize(char *str){
+	register int counter=0;
+	for (; counter<11; counter++){
+		*(str+counter)=__rand_char();
+	}
+}
+int main(void){
+	srand(time(0)); //initializing your mother's PRNG with seed.
+	char str[12];
+	memset(str,0,sizeof(char)*12/sizeof(char));
+	str[11]='\0';
+	char cmpstr[]="Hello World";
+	while(strcmp(cmpstr,str)!=0){
+		randomize(str);
+		puts(str);
+		sleep(1); //added this for torturing purposes.
+	}
+	return 0;
+}
