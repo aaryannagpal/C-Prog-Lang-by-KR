b85165f202b1a94a7c2dbe2e30931c370a2cdbb5
Sameer Achhab <sameerkw23@gmail.com>
2022-10-03 21:36:21 +0530

    Additional information about Macros.

--- Chapter_1/Example_1-4.c
+++ Chapter_1/Example_1-4.c
@@ -11,3 +11,13 @@ int main(){
         
 }
 /*They are not declarations. They are written in CAPS*/
+/*NOTE BY ANOTHER C DEVELOPER: They don't have to be in caps.
+Some things you can do with macros:-
+1. #ifndef "if not defined, do this"
+2. #ifdef "if defined, do this"
+3. #else "otherwise, do this"
+4. #undef "I don't want this macro anymore. Remove it."
+4. Create inline functions, for example:-
+#define __ALLOC_N(_P) _P=(struct node*)malloc(sizeof(struct node)); \
+                                       _P==NULL?perror("[malloc]"):puts("Allocation successful.")
+\ was used to tell the compiler that the line below it is also a part of the inline function.
