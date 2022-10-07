35fd32293b7be80ab6934747d1487d9e78109a12
Aaryan Nagpal <aaryannagpal65@gmail.com>
2022-10-08 03:03:37 +0530

    No codes only text

---
+++ Chapter_1/Exercises/1-11.c
@@ -0,0 +1,11 @@
+/*
+
+Would use many test cases
+
+1. No input; no lines, characters or words
+2. Only one kind of input (A long word, or many new lines etc)
+3. Using words containing symbols etc
+4. Give large input values to test the boundary conditions
+
+
+*/
