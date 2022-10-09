ff13b5f3f7d65ea7cdda0cb802e3c8de49a6e4e5
Aaryan Nagpal <aaryannagpal65@gmail.com>
2022-10-09 17:19:04 +0530

    Code done - Vertical Histogram

--- Chapter_1/Exercises/1-13.c
+++ Chapter_1/Exercises/1-13.c
@@ -1,76 +1,51 @@
-/*Histogram of the lengths of words in input*/
 #include <stdio.h>
-#define MAXWORD 11 //max length of a word
-#define maxhist 15 //max length of histogram
-#define IN 1    //inside a word
-#define OUT 0   //outside a word
 
+#define MAX 15
 
 int main(){
-    int word[MAXWORD];
-    int c, i, j, nc, state, maxvalue, overflow;
+    int letter[MAX];
+    int c, nc,i;
 
-    i = j = c = 0;
-    nc = 0; //number of characters in a word
-    overflow = 0; //to count overflow words
-    state = OUT;
+    nc = 0;
 
-
-    for (i = 0; i<MAXWORD; ++i){
-        word[i] = 0;
+    for (i = 0; i< MAX; ++i){
+        letter[i] = 0;
     }
 
-    while ((c = getchar())!= EOF){
-
-        if (c == ' ' || c == '\n' || c == '\t'){
-            state = OUT;
-            
-            if (nc > 0 && nc < MAXWORD){
-                ++word[nc];
-            }
-            else
-            {
-                ++overflow;
-            }
-        nc = 0;
+    while(1){
+        c = getchar();
+        if (c == EOF){
+            break;
         }
-        else if (state == OUT){
-            state = IN;
-            nc = 1; //beginning of a word
+        if (c == ' '|| c == '\n' || c == '\t'){
+            ++letter[nc];
+            nc = 0;
+            continue;
         }
-        else{            
-            nc++; //inside a word
-        }
-    }   
-
-    maxvalue = 0;
-    for (i = 1; i < MAXWORD; ++i){
-        if (word[i] > maxvalue){
-            maxvalue = word[i];
-        }
-
-    //printing histogram
-
-    int len; //length of each bar
+        ++nc;
+    }
     
-    for (i = 1; i < MAXWORD+1; ++i){
-        printf("%5d - %5d : ", i, word[i]);
-
-        if (word[i] > 0){
-            if ((len = word[i]*maxhist / maxvalue) <= 0){
-                len = 1;
+    int t;
+    for (i = 1; i< MAX; ++i){
+        if (i < 10){
+            printf(" %d  |", i);
+            for (t = 0; t != letter[i]; ++t){
+                printf("*");
+                printf("*");
             }
+            printf("\n");
         }
-        else {
-            len = 0;
+        else{
+            printf(" %d |", i);
+            for (t = 0; t != letter[i]; ++t){
+                printf("*");
+                printf("*");
+            }
+            printf("\n");
         }
-        while (len > 0){
-            putchar('*');
-            --len;
-        }
-        putchar('\n');
-        }
-
+        
+        
     }
-    return 0;
+
+    printf("Number of characters with no letters: %d\n", letter[0]);
 }
