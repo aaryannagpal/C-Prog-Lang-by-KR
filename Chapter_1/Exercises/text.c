07d2d0ec33b9bf85cedd72c7363ab29d7c4ef328
Sameer Achhab <sameerkw23@gmail.com>
2022-10-09 16:43:21 +0530

    Buggy. Debug.
    Output is erratic.

--- Chapter_1/Exercises/text.c
+++ Chapter_1/Exercises/text.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
 #define MAX 15
-
 int main(){
     int letter[MAX];
-    int c, nc,i;
+    int nc,i;
+	char c;
     nc = 0;
     for (i = 0; i< MAX; ++i){
         letter[i] = 0;
     }
-
-    while((c = getchar() != EOF)){
-        ++nc;
+    while(1){
+		c=getchar();
+		if(c=='\n'){
+			break;
+		}
         if(c == ' '){
-            ++letter[nc];
-            nc = 0;
+            letter[nc]++;
+            nc=0;
+			continue;
         }
+		nc++;
     }
     for (i = 0; i< MAX; ++i){
         printf("%d ", letter[i]);
