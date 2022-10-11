8aa47593d7d069d051da1d3027f906ef0753ca75
Aaryan Nagpal <aaryannagpal65@gmail.com>
2022-10-12 03:03:39 +0530

    F to C function

--- Chapter_1/Exercises/1-15.c
+++ Chapter_1/Exercises/1-15.c
@@ -3,12 +3,17 @@
 #include <stdio.h>
 
 float Fahr_to_Cel(float);
+float Cel_to_Fahr(float);
 
 int main(){
     int i;
     
-    for (i = 0; i < 300; i+=30){
+    for (i = 0; i < 330; i+=30){
         printf("%d F in Celsius is %.2f C\n",i, Fahr_to_Cel(i));
+    }
+
+    for (i = 0; i < 110; i+=10){
+        printf("%d C in Celsius is %.2f F\n",i, Cel_to_Fahr(i));
     }
     return 0;
 }
@@ -19,4 +24,10 @@ float Fahr_to_Cel(float F){
     float C;
     C = (5.0/9.0)*(F-32.0);
     return C;
+}
+
+float Cel_to_Fahr(float C){
+    float F;
+    F = ((9.0/5.0)*C)+32.0;
+    return F;
 }
