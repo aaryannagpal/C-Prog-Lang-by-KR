5f54acac59eed6f7d4ddc9327dd2d8646838df53
Aaryan Nagpal <aaryannagpal65@gmail.com>
2022-10-31 21:30:49 +0530

    Input larger than 80

---
+++ Chapter_1/Exercises/1-17.c
@@ -0,0 +1,42 @@
+/*Print input lines larger than 80 characters*/
+
+#include <stdio.h>
+#define MAX 1000 //maximum length of a line
+#define LIMIT 80 
+
+int getline(char a[], int lim);
+
+int main(){
+    char line[MAX];
+    int len;
+    
+    while ((len = getline(line, MAX))>0){
+        if (len > LIMIT){
+            printf("%s", line);
+        }
+    }
+    
+    return 0;
+}
+
+int getline(char s[], int lim){
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar())!= EOF && c != '\n'; ++i){
+        s[i] = c;
+    }
+    if (c == '\n'){
+        s[i] = c;
+        i++;
+    }
+    s[i] = '\0';
+    return i;
+}
+void copy(char to[], char from[]){ //void means no value is to be returned
+    int i;
+    i = 0;
+
+    while((to[i] = from[i])!= '\0'){
+        ++i;
+    }
+}
