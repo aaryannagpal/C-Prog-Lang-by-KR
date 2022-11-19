4958b14d1be7005524c1b8bdc9949da44c65c46a
aaryannagpal <aaryannagpal65@gmail.com>
2022-11-19 17:27:06 +0530

    Reverse a line per input

---
+++ Chapter_1/Exercises/1-19.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int getLine(char a []){
+    int c, i;
+    for (i = 0; i < MAX -1 && (c = getchar())!= EOF && c!= '\n'; i++){
+        a[i] = c;
+    }
+    if (c == '\n'){
+        a[i] = c;
+        i++;
+    }
+    a[i] = '\0';
+    return i;
+    //returns length of line as well as copies line in an array
+}
+
+void Reverse(char a[], int len){
+    char temp[len];
+    int i = 0;
+
+    if (a[0] == '\n'){
+        return;
+    }
+    for (i; i < len; i++){
+        temp[len - 2 - i] = a[i];
+    }
+
+    temp[len-1] = '\0';
+    printf("%s", temp);
+
+}
+int main(){
+    char line[MAX];
+    int len;
+
+    while((len = getLine(line)) > 0){
+        Reverse(line, len);
+    }
+
+    return 0;
+}
+
